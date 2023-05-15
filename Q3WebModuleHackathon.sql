322fd277246d9b59515d2544de05a262b95ed582
Vivek Ranjan <90482266+VivekRanjan001@users.noreply.github.com>
2023-05-15 15:44:47 +0530

    Add files via upload

---
+++ Q3WebModuleHackathon.sql
@@ -0,0 +1,17 @@
+use DB
+Create table tblEmpl(
+empId int primary key identity(1001, 1),
+empName varchar(50) NOT NULL,
+EMpAddress varchar(MAX)NOT NULL,
+manId int,
+empSalary money NOT NULL
+
+
+
+
+
+select A.empName ,
+B.empName from employeeMan A,
+employeeMan B where B.empId=A.manId order by A.empName;
+
+
